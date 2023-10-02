ed5992e14c0d8ab8a126e4d216233f089d7fa1cd
Daniel <virusxd521@gmail.com>
2023-10-02 07:21:52 +0300

    Finished the first version of the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
-// import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
+import { LogIn } from "lucide-react";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+  const isAuth = !!userId;
+
   return (
     <div className="w-screen min-h-screen bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
@@ -11,8 +15,27 @@ export default function Home() {
             <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
             <UserButton afterSignOutUrl="/" />
           </div>
+
           <div className="flex mt-2">
-            <Button>Go to chats</Button>
+            {isAuth && <Button>Go to chats</Button>}
+          </div>
+
+          <p>
+            Join millions of students, researchers and professionals to
+            instantly answer questions and understand research with AI
+          </p>
+
+          <div className="w-full mt-4">
+            {isAuth ? (
+              <h1>file upload</h1>
+            ) : (
+              <Link href="/sign-in">
+                <Button>
+                  Login to get started
+                  <LogIn className="w-4 h-4 ml-2" />
+                </Button>
+              </Link>
+            )}
           </div>
         </div>
       </div>
